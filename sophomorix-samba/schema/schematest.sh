d63493e8e5ed231cdc6b7d4747b86d4bae7979bb
Rüdiger Beck <jeffbeck@web.de>
2018-05-12 19:34:24 +0200

    separated Computer and User

--- sophomorix-samba/schema/schematest.sh
+++ sophomorix-samba/schema/schematest.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+
+# file 1_....
 echo "Attributes (sorted):"
 grep lDAPDisplayName -r ./1_sophomorix-attributes.ldif | sort
 grep lDAPDisplayName -r ./1_sophomorix-attributes.ldif | sort | wc -l ;
@@ -13,3 +16,10 @@ grep schemaIDGUID -r ./1_sophomorix-attributes.ldif |sort | uniq -D
 echo "Duplicate lines for attributeID (no output means: no error):"
 grep attributeID -r ./1_sophomorix-attributes.ldif |sort | uniq -D
 
+
+# file 2_....
+echo "Duplicate lines for governsID (no output means: no error):"
+grep governsID -r ./2_sophomorix-classes.ldif |sort | uniq -D
+
+echo "Duplicate lines for schemaIDGUID (no output means: no error):"
+grep schemaIDGUID -r ./2_sophomorix-classes.ldif |sort | uniq -D
