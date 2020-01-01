591b5c041dd09ebc2e04fdc7a7cf197c6a014241
Rüdiger Beck <jeffbeck@web.de>
2020-01-01 23:58:25 +0100

    prepared tests for 2020

--- sophomorix-developer/modules/SophomorixTest.pm
+++ sophomorix-developer/modules/SophomorixTest.pm
@@ -690,7 +690,7 @@ sub AD_test_object {
             my $date=$entry->get_value ('sophomorixCreationDate');
             $test_date=substr($date,0,4); # first 4 chars
             # set string ok
-            my $strg_ok="2019";
+            my $strg_ok="2020";
             if ($s_creationdate eq "exists"){
                 # test first 4 digits
                 is ($test_date,$strg_ok,
@@ -707,7 +707,7 @@ sub AD_test_object {
             my $date=$entry->get_value ('sophomorixTolerationDate');
             $test_date=substr($date,0,4); # first 3 chars
             # set string ok
-            my $strg_ok="2019";
+            my $strg_ok="2020";
             if ($s_tolerationdate eq "exists"){
                 # test first 4 digits
                 is ($test_date,$strg_ok,
@@ -725,7 +725,7 @@ sub AD_test_object {
             my $date=$entry->get_value ('sophomorixDeactivationDate');
             $test_date=substr($date,0,4); # first 4 chars
             # set string ok
-            my $strg_ok="2019";
+            my $strg_ok="2020";
             if ($s_deactivationdate eq "exists"){
                 # test first 3 digits
                 is ($test_date,$strg_ok,
