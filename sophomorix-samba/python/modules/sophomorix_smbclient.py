ac0bfe73e3e3e29da70392ffb17864fdd4b9468c
Rüdiger Beck <jeffbeck@web.de>
2022-04-13 18:40:27 +0200

    used argparse to create argument dictionary, forwarded to python function

--- sophomorix-samba/python/modules/sophomorix_smbclient.py
+++ sophomorix-samba/python/modules/sophomorix_smbclient.py
@@ -3,13 +3,14 @@ print('Loading module sophomorix_smbclient.py ...')
 
 import pprint
 
-#### functions ##############################
+############################################################
 def print_data(item,name='default_name'):
     """Übergabe mehrerer Daten"""
     print(f"{name} hat {len(name)} Buchstaben")
     print(f"{item} hat {len(item)} Buchstaben")
     return 0
 
+############################################################
 def greeting(users,username):
     """Übergabe EINER komplexen Datenstruktur"""
     #print(users) # overview of the data structure
@@ -19,5 +20,26 @@ def greeting(users,username):
     print(f"{users['sAMAccountName'][username]['firstname']} is greeted in the evening with: {users['sAMAccountName'][username]['evening_greeting']}")
     print()
 
+
+############################################################    
+def scopy(config):
+    """Übergabe mehrerer Daten"""
+    print()
+    print("The following dictionary was received:")
+    print(config)
+    print()
+    print(f"The resulting loop is:")
+    
+    for key, values in config.items():
+        print('Key :: ', key)
+        if(isinstance(values, list)):
+            for value in values:
+                source=config['from_user']+"/"+config['from_path']
+                target=value+"/"+config['to_path']
+                print(f" What to do:  copy  {source}  to  {target}")
+    return 0
+
+
+    
 
 print('... module sophomorix_smbclient.py loaded')
